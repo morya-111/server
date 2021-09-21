635566df2c891270590efd5bb8629407332d3d1f
Atharva Bhange <atharva.bhange@gmail.com>
2021-09-21 18:10:00 +0530

    typeorm/database setup complete

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
+import "reflect-metadata";
 import { config } from "dotenv";
 config();
 import app from "./app";
+import { createConnection } from "typeorm";
 
 const { PORT = 4000 } = process.env;
 
 (async () => {
+	await createConnection();
+
 	app.listen(PORT, () => {
 		console.log(`[SERVER STARTED] on http://localhost:${PORT}`);
 	});
