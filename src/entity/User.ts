78a07d3e1dafee65aec5d276d2b9ba0de030b4cd
Shreyas Gangurde <57534229+shreyasg-git@users.noreply.github.com>
2021-11-30 23:50:03 +0530

    get my books route done.

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,39 +1,44 @@
 import {
-	Entity,
-	Column,
-	PrimaryGeneratedColumn,
-	OneToOne,
-	BaseEntity,
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  OneToOne,
+  BaseEntity,
+  OneToMany,
 } from "typeorm";
 import { Address } from "./Address";
 import { Auth } from "./Auth";
 import { IsDefined, IsEmail } from "class-validator";
+import { Book } from "./Book";
 
 @Entity()
 export class User extends BaseEntity {
-	@PrimaryGeneratedColumn()
-	id: number;
+  @PrimaryGeneratedColumn()
+  id: number;
 
-	@IsDefined({ message: "First Name is required." })
-	@Column({ length: 30 })
-	first_name: string;
+  @IsDefined({ message: "First Name is required." })
+  @Column({ length: 30 })
+  first_name: string;
 
-	@IsDefined({ message: "Last Name is required." })
-	@Column({ length: 30 })
-	last_name: string;
+  @IsDefined({ message: "Last Name is required." })
+  @Column({ length: 30 })
+  last_name: string;
 
-	@IsDefined({ message: "Email is required." })
-	@IsEmail()
-	@Column({ length: 200, unique: true })
-	email: string;
+  @IsDefined({ message: "Email is required." })
+  @IsEmail()
+  @Column({ length: 200, unique: true })
+  email: string;
 
-	@IsDefined()
-	@Column({ length: 20 }) // This can be later set to enum when all roles are finalized
-	role: string;
+  @IsDefined()
+  @Column({ length: 20 }) // This can be later set to enum when all roles are finalized
+  role: string;
 
-	@OneToOne(() => Auth, (auth) => auth.user)
-	auth: Auth;
+  @OneToOne(() => Auth, (auth) => auth.user)
+  auth: Auth;
 
-	@OneToOne(() => Address, (address) => address.user)
-	address: Address;
+  @OneToOne(() => Address, (address) => address.user)
+  address: Address;
+
+  @OneToMany(() => Book, (book) => book.user)
+  books: Book[];
 }
--- src/routes/bookRoutes.ts
+++ src/routes/bookRoutes.ts
@@ -3,10 +3,15 @@ import {
   getAllBooks,
   newBook,
   getBookById,
+  getAllBooksByLoggedInUser,
 } from "../controllers/bookController";
+
+import { protect } from "../controllers/authController";
+
 const bookRouter = Router();
 
+bookRouter.get("/mybooks", protect(), getAllBooksByLoggedInUser);
 bookRouter.get("/", getAllBooks).get("/:id", getBookById);
-bookRouter.post("/", newBook);
+bookRouter.post("/", protect(), newBook);
 
 export default bookRouter;
