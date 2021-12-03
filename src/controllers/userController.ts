7c78b5d5e556ac32472c4b5d9114b809c173254e
Shreyas Gangurde <57534229+shreyasg-git@users.noreply.github.com>
2021-12-03 13:36:36 +0530

    myprofile routes added

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,16 +1,15 @@
 import { validate } from "class-validator";
 import { RequestHandler } from "express";
-import { userInfo } from "os";
-//<img src="https://i.ibb.co/svPq37Q/62c39aa27b5f.png" alt="62c39aa27b5f" border="0">
+import { compare } from "bcryptjs"; //<img src="https://i.ibb.co/svPq37Q/62c39aa27b5f.png" alt="62c39aa27b5f" border="0">
 import { User } from "../entity/User";
 import AppError from "../utils/AppError";
+import { hashSync } from "bcryptjs";
 
 export const updateUser: RequestHandler = async (req, res, next) => {
   const newUser = req.body;
-  console.log(newUser);
 
   const userId = (<any>req.user).id;
-  const oldUser = await User.findOne({ where: { id: userId } });
+  const oldUser = await User.findOne({ id: userId }, { relations: ["auth"] });
 
   const changedFields: string[] = [];
 
@@ -32,8 +31,30 @@ export const updateUser: RequestHandler = async (req, res, next) => {
     oldUser.last_name = newUser.last_name;
     changedFields.push("last_name");
   }
+
+  if (newUser.password) {
+    if (oldUser.auth.password) {
+      return next(
+        new AppError(`User has not signed up using BookEx Auth System `, 404)
+      );
+    } else if (await compare(newUser.password, oldUser.auth.password)) {
+      return next(new AppError(`New Password can not be same as before `, 401));
+    } else {
+      oldUser.auth.password = newUser.password;
+      const authVal = await validate(oldUser.auth);
+      if (authVal.length > 0) {
+        return next(
+          new AppError(`Validation Errors in new password`, 401, authVal)
+        );
+      }
+      changedFields.push("password");
+
+      oldUser.auth.password = hashSync(oldUser.auth.password, 12);
+      await oldUser.auth.save();
+    }
+  }
+
   const valErrors = await validate(oldUser);
-  console.log(valErrors);
 
   if (valErrors.length > 0) {
     return next(new AppError("Validation Errors", 400, valErrors));
@@ -45,10 +66,30 @@ export const updateUser: RequestHandler = async (req, res, next) => {
 
   await oldUser.save();
 
+  oldUser.auth = undefined;
+
   return res.status(200).json({
     message: "success",
     changedFields,
-    data: { oldUser },
+    data: { newUser: oldUser },
+  });
+};
+
+export const getLoggedInUsing: RequestHandler = async (req, res, next) => {
+  const userId = (<User>req.user).id;
+  const user = await User.findOne(userId, { relations: ["auth"] });
+  let loggedInUsing: string;
+  if (user.auth.password) {
+    loggedInUsing = "BOOKEX";
+  } else if (user.auth.facebook_id) {
+    loggedInUsing = "FB";
+  } else if (user.auth.google_id) {
+    loggedInUsing = "GOOGLE";
+  }
+
+  res.status(200).json({
+    message: "success",
+    data: { loggedInUsing },
   });
 };
 
