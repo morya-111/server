ba2e1ca4a0e8be9077dd0dc171e2eff7a536ae2b
Atharva Bhange <athhb@pop-os.localdomain>
2021-12-06 17:47:36 +0530

    search field added to getAllBooks, stadalone script updated add books, sellListing and borrowListing

--- src/standalone.ts
+++ src/standalone.ts
@@ -7,6 +7,9 @@ import { Book } from "./entity/Book";
 import { Language } from "./entity/Language";
 import { User } from "./entity/User";
 import { Auth } from "./entity/Auth";
+import { RentListing } from "./entity/RentListing";
+import { SellListing } from "./entity/SellListing";
+import faker, { fake } from "faker";
 
 const MAX_ROWS_TO_INSERT = 300;
 
@@ -32,22 +35,23 @@ const GENRE = [
 
 const PRIORITY_LANGUAGES = ["english", "marathi", "hindi"];
 const LANGUAGES = ["french", "spanish", "tamil", "urdu", "dutch", "hebrew"];
+const DURATION_UNITS = ["Months", "Days", "Years"];
 
 (async () => {
   await connection.create();
 
-  // await connection.clear();
+  await connection.clear();
 
-  // const user = await User.create({
-  //   email: "a@b.com",
-  //   first_name: "Chintu",
-  //   last_name: "Sharma",
-  //   role: "INDIVIDUAL",
-  // }).save();
-  // await Auth.create({
-  //   password: "12345678",
-  //   user,
-  // }).save();
+  const user = await User.create({
+    email: "a@b.com",
+    first_name: "Chintu",
+    last_name: "Sharma",
+    role: "INDIVIDUAL",
+  }).save();
+  await Auth.create({
+    password: "qwertyui",
+    user,
+  }).save();
 
   const languageObjects = [];
 
@@ -65,35 +69,73 @@ const LANGUAGES = ["french", "spanish", "tamil", "urdu", "dutch", "hebrew"];
     languageObjects.push(lang);
   }
 
-  // const stream = fs
-  //   .createReadStream(path.resolve(__dirname, "../dev-data/500-books.csv"))
-  //   .pipe(csv.parse({ headers: true, maxRows: MAX_ROWS_TO_INSERT }))
-  //   .on("error", async (error) => {
-  //     console.error(error);
-  //     await connection.close();
-  //   })
-  //   .on("data", async (row) => {
-  //     try {
-  //       stream.pause();
-  //       const image = await Image.create({ url: row.img }).save();
-  //       const book = await Book.create({
-  //         name: row.name,
-  //         author: row.author,
-  //         publisher: row.publication,
-  //         description: row.description === "" ? null : row.description,
-  //         genre: GENRE[Math.floor(Math.random() * GENRE.length)],
-  //         language:
-  //           languageObjects[Math.floor(Math.random() * languageObjects.length)],
-  //         image,
-  //         user,
-  //       }).save();
-  //     } finally {
-  //       stream.resume();
-  //     }
-  //   })
-  //   .on("end", async () => {
-  //     setTimeout(async () => {
-  //       await connection.close();
-  //     }, 3000);
-  //   });
+  const stream = fs
+    .createReadStream(path.resolve(__dirname, "../dev-data/500-books.csv"))
+    .pipe(csv.parse({ headers: true, maxRows: MAX_ROWS_TO_INSERT }))
+    .on("error", async (error) => {
+      console.error(error);
+      await connection.close();
+    })
+    .on("data", async (row) => {
+      try {
+        stream.pause();
+        const image = await Image.create({ url: row.img }).save();
+        const book = await Book.create({
+          name: row.name,
+          author: row.author,
+          publisher: row.publication,
+          description: row.description === "" ? null : row.description,
+          genre: GENRE[Math.floor(Math.random() * GENRE.length)],
+          language:
+            languageObjects[Math.floor(Math.random() * languageObjects.length)],
+          image,
+          user,
+        }).save();
+
+        const decider = Math.random();
+
+        if (decider > 0.3 && decider < 0.6) {
+          // add sell listing
+          const sellListing = await SellListing.create({
+            price: faker.datatype.float(2),
+            book,
+          }).save();
+        } else if (decider >= 0.6 && decider < 0.9) {
+          // add rent listing
+          console.log("ADDING RENT LISTING");
+
+          const rentListing = await RentListing.create({
+            book,
+            deposit: faker.datatype.float(2),
+            duration: faker.datatype.number(20),
+            fees: faker.datatype.float(2),
+            durationUnit:
+              DURATION_UNITS[Math.floor(Math.random() * DURATION_UNITS.length)],
+          }).save();
+        } else if (decider >= 0.9) {
+          // add both sell and rent listing
+
+          const sellListing = await SellListing.create({
+            price: faker.datatype.float(2),
+            book,
+          }).save();
+
+          const rentListing = await RentListing.create({
+            book,
+            deposit: faker.datatype.float(2),
+            duration: faker.datatype.number(20),
+            fees: faker.datatype.float(2),
+            durationUnit:
+              DURATION_UNITS[Math.floor(Math.random() * DURATION_UNITS.length)],
+          }).save();
+        }
+      } finally {
+        stream.resume();
+      }
+    })
+    .on("end", async () => {
+      setTimeout(async () => {
+        await connection.close();
+      }, 3000);
+    });
 })();
